546905181b9ea7866b60afabfc47edc27f66c90c
Stan <stan@gmail.com>
2021-06-03 18:07:17 -0500

    наконец заработали отступы

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,31 @@ const { nanoid } = require("nanoid")
 
 const idlength =8;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The new Turing Omnibus
+ *         author: Alexandr K. Dendney
+ */
+
 router.get("/", (req, res) => {
     const books = req.app.db.get("books")
 
